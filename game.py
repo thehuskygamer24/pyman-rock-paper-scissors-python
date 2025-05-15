f1fecf6bc39c059060b039112b62ed9b33aaf4fc
supersaiyanslasher <166193944+thehuskygamer24@users.noreply.github.com>
2025-05-15 14:19:17 -0400

    Update game.py

--- game.py
+++ game.py
@@ -3,10 +3,19 @@ import time
 import requests
 from requests.exceptions import Timeout
 from requests.exceptions import ConnectionError
+accountuser = ""
 print("use the input below to use 1 for rock 2 for paper 3 for scissors")
 gamehub = ["rock","paper", "scissors", "paper","scissors","rock","scissors","paper","rock","rock","scissors","rock"]
-clientversion = "0.3.2"
+p2 = requests.get("http://localhost:3007/accountget")
+if p2 == "":
+  pass
+else:
+ accountuser = p2.text.strip()
+clientversion = "0.2.5"
+
 startgameapproval = True
+
+
 def startgame():
  while True:
   userinput = float(input("1 2 3. "))
@@ -43,13 +52,18 @@ def serverconnect():
    else:
      print("client outdated please update the software https://github.com/thehuskygamer24/pyman-rock-paper-scissors-python")
      startgameapproval = False
-     time.sleep(9999999999999999)
+     time.sleep(30000000)
    time.sleep(10)
-   print("server connection successful you are signed in as a guest")
- except Timeout:
-   print("theres no active server connection")
-   startgameapproval = False
-   time.sleep(30000)
+   if accountuser == "":
+    print("you need to create your userid")
+    m1 = input("").strip()
+    requests.post("http://localhost:3007/accountadd", data=m1)
+   
+  
+  
+    print("server connection successful you are signed in as "+ m1)
+   else:
+    print("server connection successful you are signed in as "+ p2.text)
  except ConnectionError:
    print("server connection failed or no internet")
    startgameapproval = False
