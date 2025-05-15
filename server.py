52bda8c7bcebe92dacfc943cd24427432e4bb25c
supersaiyanslasher <166193944+thehuskygamer24@users.noreply.github.com>
2025-05-15 13:13:38 -0400

    Update server.py

--- server.py
+++ server.py
@@ -1,19 +1,21 @@
 from flask import Flask 
 import requests
 a1 = requests.get("https://raw.githubusercontent.com/thehuskygamer24/pyman-rock-paper-scissors-python/refs/heads/main/serveractive.txt")
-app = Flask(__name__)
-serverversion = "0.2.3"
+if a1.text == "false":
+ exit()
+else:
+ app = Flask(__name__)
+  serverversion = "0.2.3"
 @app.route("/")
-def connectivity():
-  return "connection success"
-@app.route("/version")
-def version():
+ def connectivity():
+   return "connection success"
+ @app.route("/version")
+ def version():
   return serverversion
-@app.route("/update")
-def heartbeat():
+ @app.route("/update")
+ def heartbeat():
   return "heartbeat1"
 if __name__ == "__main__":
   
- app.run(host="0.0.0.0", port=3007)
-elif a1.text == "false":
-  exit()
+  app.run(host="0.0.0.0", port=3007)
+
