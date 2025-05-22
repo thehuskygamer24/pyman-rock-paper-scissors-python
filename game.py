54b92b71a23d0fce34c17ab13c7a13498faa3610
supersaiyanslasher <166193944+thehuskygamer24@users.noreply.github.com>
2025-05-22 19:11:38 -0400

    Update game.py

--- game.py
+++ game.py
@@ -2,42 +2,51 @@ import random
 import time
 import requests
 import os
+
 from playsound import playsound
 os.system("title Pyman RPS Cmd Game")
 startgameapproval = True
-from colorama import Fore,init
-init(autoreset=True)
-from requests.exceptions import ConnectionError
-getgamestate = requests.get("https://raw.githubusercontent.com/thehuskygamer24/pyman-rock-paper-scissors-python/refs/heads/main/shutdownactivetxt")
-getgamestatereason = requests.get("https://raw.githubusercontent.com/thehuskygamer24/pyman-rock-paper-scissors-python/refs/heads/main/shutdownreason.txt")
-if getgamestate.text.strip() == "active":
-  startgameapproval = False
-  print(Fore.RED + "pyman RPS services are unavailable " + "reason: ", end='')
-  print(Fore.LIGHTRED_EX + getgamestatereason.text.strip())
-  time.sleep(3000000)
-  exit()
-else:
-  pass
+from colorama import Fore
 
-accountuser = "Blue Team"
+from requests.exceptions import ConnectionError
+
+
+
+
+
+
 print(Fore.YELLOW + "use the input below to use 1 for rock 2 for paper 3 for scissors")
-gamehub = ["rock","paper", "scissors", "paper","scissors","rock","scissors","paper","rock","rock","scissors","paper"]
-##if p2 == "":
-  #pass
-#else:
- #accountuser = p2.text.strip()
-clientversion = "0.4.1"
+gamehub = ["rock","paper", "scissors", "paper","scissors","rock","scissors","paper"]
+
+
+
+clientversion = "0.3.4"
+pathr = "system/New folder (7)/fgdt.txt"
+b6 =open(pathr)
+b7 = b6.read()
+if not b7 == "":
+  pass
+  
+else:
+ 
+ cms = random.randint(1,100000)
+
+ 
+
+ cm = open(pathr, "w")
+ cm.write(str(cms))
+ cm.close()
 
 
 def musicintro():
-  playsound("soundtracks/westminsterintro2.mp3")
+  playsound("soundtracks/intromusic.mp3")
 
 def startgame():
  while True:
   
   userinput = float(input(Fore.LIGHTBLUE_EX + "1 2 3. "))
   try:
-   requests.get("http://192.168.1.65:3007/update")
+   requests.get("https://192.168.1.65:3007/update",verify='auth/cert.pem')
   
   
    time.sleep(0.7)
@@ -46,7 +55,7 @@ def startgame():
     time.sleep(3)
     print(Fore.RED + "unable to connect to a session code 3")
     time.sleep(10)
-    print(Fore.RED + "server connection stopped unexpectly please relaunch the game and try again")
+    print(Fore.RED + "server connection interupted unexpectly please relaunch the game and try again")
     startgameapproval = False
     time.sleep(300000)
   randomshoot = random.choice(gamehub)
@@ -74,49 +83,87 @@ def startgame():
    
     
   print(Fore.RED + "cpu's response " + randomshoot)
+
 def serverconnect():
  try:
-   requests.get("http://192.168.1.65:3007", timeout=7)
-   c1 = requests.get("http://192.168.1.65:3007/version")
+   requests.get("https://192.168.1.65:3007", verify='auth/cert.pem')
+   c1 = requests.get("https://192.168.1.65:3007/version", verify='auth/cert.pem')
    if c1.text == clientversion:
     pass
    else:
      print(Fore.RED + "client outdated please update the software https://github.com/thehuskygamer24/pyman-rock-paper-scissors-python")
-     print(Fore.YELLOW + "be aware the client will not bypass the update requirement")
-     print(Fore.YELLOW + "if there is an issue with updating pyman or version not set correctly between the client server or both then please contact @supersaiyanslasher on discord the owner of the game")
+     print(Fore.LIGHTRED_EX + "be aware the client will not bypass the update requirement")
+     print(Fore.LIGHTRED_EX + "if there is an issue with updating pyman or version not set correctly between the client server or both then please contact @supersaiyanslasher on discord the owner of the game")
      startgameapproval = False
      time.sleep(30000)
-   time.sleep(3)
-   if accountuser == "":
-    print("you need to create your userid")
-    m1 = input("").strip()
-    requests.post("http://192.168.1.65:3007/accountadd", data=m1)
+   time.sleep(1)
+ 
    
   
   
-    print(Fore.GREEN + "server connection successful you are signed in as "+ "Blue Team")
-    print(Fore.BLUE + "your team Blue")
-   else:
-    print(Fore.GREEN + "server connection successful you are signed in as "+ "Blue Team")
-    print(Fore.BLUE + "your team Blue")
+   print(Fore.LIGHTYELLOW_EX + "server connection successful you are signed in as "+ "Player Blue")
+   print(Fore.BLUE + "your team Blue")
+   
  except ConnectionError:
    print(Fore.RED + "failed to connect to RPS.NET error code: outage")
    startgameapproval = False
    time.sleep(3000000)
+def mainmenu():
+  
+  print("(1) Play Game")
+  print("(2) change username")
+  print("(3) gallery")
+  print("(4) staff contact")
+  print("(5) settings")
+  vm1 = input('')
+  if vm1 == '1':
+    print("Its Game Time")
+    time.sleep(3)
+    print(Fore.LIGHTGREEN_EX + "your command pallet has been enabled have fun :)")
+    startgame()
+  elif vm1 == '2':
+    print("usernames systems Permanentley removed")
+    
+    time.sleep(3)
+    mainmenu()
+  elif vm1 == "3":
+    print("(1) Top wins")
+    print("(2) ultimate users who played")
+    print("by discord username")
+    
+    print("gallery update coming soon")
+    input("back. ")
+    mainmenu()
+  elif vm1 == "4":
+    print("contact staff at https://discord.gg/CCMyCN2qBR")
 
+    input("back. ")
+    mainmenu()
+
+
+  elif vm1 == "5":
+    print("settings feature coming soon")
+    time.sleep(3)
+    mainmenu()
+  else:
+    pass
 
 print(Fore.BLUE + "welcome to pyman rock paper scissors")
 time.sleep(1)
 print(Fore.CYAN + "connecting to server")
 serverconnect()
+
+
 if startgameapproval == False:
   pass
   
 else:
- time.sleep(1)
+
  
- musicintro()
+ 
+ mainmenu()
  time.sleep(3)
- startgame()
- print("your command pallet has been enabled have fun :)")
+ 
+
+
 
